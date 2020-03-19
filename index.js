a4fde3c6f4a83f600da037ea25465baf0349dcf4
sesamestrong <doolie.healey@gmail.com>
2020-03-19 19:32:53 -0400

    Add NODE_ENV=development requirement

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const timerMaker=()=>{
     let tempTime=new Date();
-    const nextTime=name=>{
+    const nextTime=process.env.NODE_ENV=="development"?(name=>{
         const newTime=new Date();
         console.log(name,"took",newTime-tempTime,"ms");
         tempTime=newTime;
-    };
+    }):()=>null;
     return nextTime;
 };
 let timer=timerMaker();
